88e6729134b526691a3e538db600e233dd435fac
Marcelo Amorim <marceloamorim.fa@gmail.com>
2024-12-30 20:34:35 +0000

    Django Templates - facker and for cicle

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
 def home(request):
     return render(
         request,
         'recipes/pages/home.html',
+        {
+            'recipes': [make_recipe() for _ in range(10)]
+        }
     )
 
 def recipe(request, id_recipe):
@@ -13,6 +17,6 @@ def recipe(request, id_recipe):
         request,
         'recipes/pages/recipe-view.html',
         {
-            'author': 'Marcelo Amorim'
+            'recipe': make_recipe()
         }
     )
